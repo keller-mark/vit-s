8646099b7f73beec8f76e07bffa450d27c7d2d20
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-06-01 12:28:57 -0400

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,44 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import replace from 'rollup-plugin-replace';
 import { defineConfig } from 'vite';
 
+console.log(process.env.NODE_ENV);
+
 export default defineConfig({
-    plugins: [
-      react({
-        jsxRuntime: 'classic',
-        babel: {
-          "presets": [
-            "react-app"
-          ],
-          "plugins": [
-            "glsl"
-          ]
-        }
-      }),
-    ],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.js'),
-            name: 'VitS',
-            formats: ['es', 'umd'],
-            fileName: (format) => `vit-s.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                },
-            },
-        },
+  plugins: [
+    react({
+      jsxRuntime: 'classic',
+      babel: {
+        "presets": [
+          "react-app"
+        ],
+        "plugins": [
+          "glsl"
+        ]
+      }
+    }),
+  ],
+  build: {
+    lib: {
+        entry: path.resolve(__dirname, 'src/index.js'),
+        name: 'VitS',
+        formats: ['es', 'umd'],
+        fileName: (format) => `vit-s.${format}.js`,
     },
+    rollupOptions: {
+      plugins: [
+        replace({
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        }),
+      ],
+      external: ['react', 'react-dom'],
+      output: {
+          globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+          },
+      },
+    },
+  },
 });
